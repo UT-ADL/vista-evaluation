029b67f9deb395c82738ec076291f8aff122501d
Mykyta Baliesnyi <nikita.balesny@gmail.com>
2022-09-05 11:24:56 +0300

    faster bag extraction, lighter video compression

--- rosbag_to_vista.py
+++ rosbag_to_vista.py
@@ -1,209 +1,126 @@
 import os
 import argparse
+import socket
+import time
 
-from bagpy import bagreader
 import pandas as pd
 import numpy as np
+from video_extractor import VideoStream, compressed_imgmsg_to_cv2
+import rosbag
+from tqdm import tqdm
+
+BOLT_DIR = '/data/Bolt' if socket.gethostname() == 'neuron' else '/gpfs/space/projects/Bolt'
+BAGS_DIR = os.path.join(BOLT_DIR, 'bagfiles')
+
+CAMERA_TOPIC = '/interfacea/link2/image/compressed'
+SPEED_TOPIC = '/ssc/velocity_accel_cov'
+YAW_RATE_TOPIC = '/ssc/curvature_feedback'
 
 
-if __name__ == '__main__':
+def read_bag(path_to_bag, topics):
+    bag = rosbag.Bag(path_to_bag, 'r')
+    msg_count = bag.get_message_count(topics)
+    progress = tqdm(total=msg_count)
 
-    parser = argparse.ArgumentParser(description='Convert a rosbag to a vista file')
-    parser.add_argument('--root', default='/gpfs/space/projects/Bolt/bagfiles/', help='Path to the bags directory')
-    parser.add_argument('--bag', required=True, help='Name of the bag file') # '2022-06-29-10-46-40_e2e_elva__forward_steering2.bag'
-    args = parser.parse_args()
+    video_stream = VideoStream()
+    frame_idx = 0
 
-    root_path = args.root_path
-    path_to_bag = os.path.join(args.root, args.bag) 
-    b = bagreader(path_to_bag)
+    camera_dict = dict()
+    speed_dict = dict()
+    curvature_dict = dict()
 
-    image_msgs = b.message_by_topic('/interfacea/link2/image/compressed')
-    df_laser = pd.read_csv(image_msgs)
+    for topic, msg, ts in bag.read_messages(topics):
+        msg_timestamp = msg.header.stamp.to_nsec()
 
-    df = pd.DataFrame({'ros_time': df_laser['Time']})
+        if topic == CAMERA_TOPIC:
+            camera_dict['time'] = msg_timestamp
+            vis_img = compressed_imgmsg_to_cv2(msg)
+            video_stream.write(vis_img, index=frame_idx)
+            frame_idx += 1
+        elif topic == SPEED_TOPIC:
+            speed_dict['time'] = msg_timestamp
+            speed_dict['speed'] = msg.velocity
+        elif topic == YAW_RATE_TOPIC:
+            curvature_dict['time'] = msg_timestamp
+            curvature_dict['curvature'] = msg.curvature
+
+        progress.update(1)
+        
+    bag.close()
+
+    return video_stream, camera_dict, speed_dict, curvature_dict
+
+def create_timestamp_index(df, timestamp_col="timestamp", index_col="index"):
+    df[timestamp_col] = pd.to_datetime(df[timestamp_col])
+    df.set_index([timestamp_col], inplace=True)
+    df.index.rename(index_col, inplace=True)
+
+def save_video(stream, output_dir):
+    output_path = os.path.join(output_dir, 'camera_front.avi')
+    stream.save(output_path)
+
+def save_video_timestamps(camera_dict, output_dir):
+    camera_df = pd.DataFrame(data=camera_dict)
+    df = pd.DataFrame({'ros_time': camera_df['Time']})
     df.index.name = '#frame_num'
-    df.to_csv('camera_front.csv')
+    df.to_csv(os.path.join(output_dir, 'camera_front.csv'))
 
-    curvature_msgs = b.message_by_topic('/ssc/curvature_feedback')
-    df_curvature = pd.read_csv(curvature_msgs)
+def save_speed(speed_dict, output_dir):
+    speed_df = pd.DataFrame(data=speed_dict, columns=["time", "speed"])
+    create_timestamp_index(speed_df, 'time')
+    speed_df.to_csv(os.path.join(output_dir, 'speed.csv'))
+    return speed_df
 
-    speed_msgs = b.message_by_topic('/current_velocity')
-    df_speed = pd.read_csv(speed_msgs)
-
-    df2 = pd.DataFrame({'time': df_speed['Time'], 'speed': df_speed['twist.linear.x'] })
-    df2.set_index('time')
-    df2.to_csv('speed.csv', index=False)
+def save_yaw_rate(curvature_dict, speed_df, output_dir):
+    curvature_df = pd.DataFrame(data=curvature_dict, columns=["time", "curvature"])
+    create_timestamp_index(curvature_df, 'time')
 
     # assume that the frequency is similar for both topics
-    print(f'Curvature messages: {len(df_curvature)}. Frequency: {1/df_curvature["Time"].diff().mean()}')
-    print(f'Speed messages: {len(df_speed)}. Frequency: {1/df_speed["Time"].diff().mean()}')
+    print(f'Curvature messages: {len(curvature_df)}. Frequency: {1/curvature_df["Time"].diff().mean()}')
+    print(f'Speed messages: {len(speed_df)}. Frequency: {1/speed_df["Time"].diff().mean()}')
 
-    min_len = min( len(df_curvature), len(df_speed) )
-    curvs = df_curvature[:min_len]
-    spdss = df_speed[:min_len]
-    yaw_rate = curvs['curvature'] * np.maximum(spdss['twist.linear.x'], 1e-10)
+    min_len = min( len(curvature_df), len(speed_df) )
+    curvs = curvature_df[:min_len]
+    spdss = speed_df[:min_len]
+    yaw_rate = curvs['curvature'] * np.maximum(spdss['speed'], 1e-15)
 
     zeros = [0]*len(spdss['Time'])
     imu_df = pd.DataFrame( {'time': spdss['Time'], 'ax': zeros, 'ay': zeros, 'az': zeros, 
-                                                   'rx': zeros, 'ry': zeros, 'rz': yaw_rate, 
+                                                   'rx': zeros, 'ry': zeros, 'rz': yaw_rate, # <----
                                                    'qx': zeros, 'qy': zeros, 'qz': zeros, 'qw': zeros})
     imu_df.set_index('time')
-    imu_df.to_csv('imu.csv', index=False)
+    imu_df.to_csv(os.path.join(output_dir, 'imu.csv'), index=False)
 
-# for topic in b.topic_table['Topics']:
-#     print( topic )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert a rosbag to a vista file')
+    parser.add_argument('--root', default=BAGS_DIR, help='Path to the bags directory')
+    parser.add_argument('--bag', required=True, help='Name of the bag file') # e.g. '2022-06-29-10-46-40_e2e_elva__forward_steering2.bag'
+    parser.add_argument('--output-base', default=os.path.join(BOLT_DIR, 'end-to-end/vista'), help='Path to the output base directory.')
+    args = parser.parse_args()
+
+    bag_name = os.path.basename(args.bag).split('.')[0]
+    output_dir = os.path.join(args.output_base, bag_name)
+    os.makedirs(output_dir, exist_ok=True)
+    path_to_bag = os.path.join(args.root, args.bag) 
+
+    topics = [CAMERA_TOPIC, SPEED_TOPIC, YAW_RATE_TOPIC] # all are 30Hz topics
     
-# LASER_MSG = b.message_by_topic('/interfacea/link2/image/compressed')
-# pd.set_option('display.max_columns', None)
-# df_laser = pd.read_csv(LASER_MSG)
-# df_laser['Time']
+    bag_read_start = time.perf_counter()
+    video_stream, camera_dict, speed_dict, curvature_dict = read_bag(path_to_bag, topics)
+    print('Done reading!')
+    bag_read_end = time.perf_counter()
 
-# df = pd.DataFrame({'ros_time': df_laser['Time']})
-# df.index.name = '#frame_num'
-# df.to_csv('camera_front.csv')
+    save_start = time.perf_counter()
+    save_video(video_stream, output_dir)
+    save_video_timestamps(camera_dict, output_dir)
+    speed_df = save_speed(speed_dict, output_dir)
+    save_yaw_rate(curvature_dict, speed_df, output_dir)
+    save_end = time.perf_counter()
+    print('Done saving!')
 
-# times_count = len(df_laser['Time'])
-# df0 = pd.DataFrame({'timestamp': df_laser['Time'], 'camera_front': range(times_count) })
-# df0.set_index('timestamp')
-# df0.to_csv('master_clock.csv', index=False)
+    bag_read_time = bag_read_end - bag_read_start
+    save_time = save_end - save_start
+    total_time = bag_read_time + save_time
 
-
-
-### ===================================================
-# import math
-# LASER_MSG = b.message_by_topic('/current_velocity')
-# pd.set_option('display.max_columns', None)
-# df_yaw = pd.read_csv(LASER_MSG)
-
-# LASER_MSG = b.message_by_topic('/novatel/oem7/inspva')
-# pd.set_option('display.max_columns', None)
-# df_yaw = pd.read_csv(LASER_MSG)
-
-# yaw_rates = []
-# prev_yaw = 0.0
-# prev_time = 0.0
-# for yaw in df_yaw:
-#     yaw_rad = math.radians(yaw['azimuth'])
-#     yaw_rate = ( yaw_rad - prev_yaw ) / ( yaw['Time'] - prev_time )
-#     yaw_rates.append( yaw_rate )
-#     prev_yaw = yaw_rad
-#     prev_time = yaw['Time']
-
-# zeros = [0]*len(df_yaw['Time'])
-# df3 = pd.DataFrame( {'time': df_yaw['Time'], 'ax': zeros, 'ay': zeros, 'az': zeros, 'rx': zeros, 'ry': zeros, 'rz': df_yaw['twist.angular.z'], 'qx': zeros, 'qy': zeros, 'qz': zeros, 'qw': zeros})
-
-# df3.set_index('time')
-# df3.to_csv('imu.csv', index=False)
-
-### ===================================================
-
-# LASER_MSG = b.message_by_topic('/novatel/oem7/corrimu')
-# pd.set_option('display.max_columns', None)
-# df_yaw_rate = pd.read_csv(LASER_MSG)
-
-# yaw_rate = df_yaw_rate['yaw_rate'] * 100
-
-# i = 0
-# print( yaw_rate[i:i+5], np.mean( yaw_rate[i:i+5] ) )
-
-
-# smooth_yawrate = []
-# for i in range( len(yaw_rate) - 5 ):
-#     smooth_yawrate.append( np.mean( yaw_rate[i:i+5] ) )
-
-# last_yaw = smooth_yawrate[-1]
-# for _ in range(5):
-#     smooth_yawrate.append(last_yaw)
-
-### ===================================================
-
-# LASER_MSG = b.message_by_topic('/ssc/curvature_feedback')
-# pd.set_option('display.max_columns', None)
-# df_curvature = pd.read_csv(LASER_MSG)
-
-
-# LASER_MSG = b.message_by_topic('/current_velocity')
-# pd.set_option('display.max_columns', None)
-# df_speed = pd.read_csv(LASER_MSG)
-
-# df2 = pd.DataFrame({'time': df_speed['Time'], 'speed': df_speed['twist.linear.x'] })
-# df2.set_index('time')
-# df2.to_csv('speed.csv', index=False)
-
-# min_len = min( len(df_curvature), len(df_speed) )
-
-# curvs = df_curvature[:min_len]
-# spdss = df_speed[:min_len]
-
-
-# yaw_rate = curvs['curvature'] * np.maximum(spdss['twist.linear.x'], 1e-10)
-
-# indxs = range( len(smooth_yawrate) )
-
-# plt.plot( indxs, smooth_yawrate )
-# indxs = range( len(yaw_rate) )
-# plt.plot( indxs, yaw_rate )
-
-# plt.savefig('yaw_rate.png')
-
-
-# zeros = [0]*len(spdss['Time'])
-# #print( f'zeros={len(zeros)} yaw={len(smooth_yawrate)}' )
-# df3 = pd.DataFrame( {'time': spdss['Time'], 'ax': zeros, 'ay': zeros, 'az': zeros, 'rx': zeros, 'ry': zeros, 'rz': yaw_rate, 'qx': zeros, 'qy': zeros, 'qz': zeros, 'qw': zeros})
-
-# df3.set_index('time')
-# df3.to_csv('imu.csv', index=False)
-
-
-# LASER_MSG = b.message_by_topic('/pacmod/parsed_tx/yaw_rate_rpt') #/novatel/oem7/corrimu
-# pd.set_option('display.max_columns', None)
-# df_laser = pd.read_csv(LASER_MSG)
-# zeros = [0]*len(df_laser['Time'])
-# df3 = pd.DataFrame( {'time': df_laser['Time'], 'ax': zeros, 'ay': zeros, 'az': zeros, 'rx': zeros, 'ry': zeros, 'rz': df_laser['yaw_rate'], 'qx': zeros, 'qy': zeros, 'qz': zeros, 'qw': zeros})
-
-# df3.set_index('time')
-# df3.to_csv('imu.csv', index=False)
-
-# Create a simple helper class that will assist us in storing videos of the render
-# class VideoStream():
-#     def __init__(self):
-#         self.tmp = "./tmp_imgs"
-#         if os.path.exists(self.tmp) and os.path.isdir(self.tmp):
-#             shutil.rmtree(self.tmp)
-#         os.mkdir(self.tmp)
-#     def write(self, image, index):
-#         cv2.imwrite(os.path.join(self.tmp, f"{index:04}.png"), image)
-#     def save(self, fname):
-#         cmd = f"/usr/bin/ffmpeg -f image2 -i {self.tmp}/%04d.png -crf 0 -y {fname}"
-#         subprocess.call(cmd, shell=True)
-
-
-#stream = VideoStream()
-
-#i_step = 0
-
-#bag = rosbag.Bag(big_bag, 'r')
-#msgs = bag.read_messages(topics=['/interfacea/link2/image/compressed'])
-
-#for m in msgs:
-#    msg = m.message
-
-#   fl = open( 'tmp.jpeg', 'wb' )
-#    fl.write( bytearray(msg.data) )
-#    fl.close()
-
-#    vis_img = cv2.imread( 'tmp.jpeg' )
-
-#    stream.write(vis_img, index=i_step)
-#    i_step += 1
-
-#stream.save("from_rosbag.avi")
-
-
-
-
-
-
-
-
-
+    print(f'\nTime spent: {total_time:.2f} seconds (reading: {bag_read_time:.2f}, saving: {save_time:.2f})')
--- video_extractor.py
+++ video_extractor.py
@@ -5,12 +5,9 @@ import rosbag
 import cv2
 import numpy as np
 
-
-root_path = '/data/Bolt/bagfiles/'
-big_bag = root_path + '2021-10-20-11-50-26_e2e_rec_vahi_06_spring2.bag' # '2021-11-17-12-29-22_e2e_rec_elva_camera.bag'
+CAMERA_TOPIC = '/interfacea/link2/image/compressed'
 
 
-# Create a simple helper class that will assist us in storing videos of the render
 class VideoStream():
     def __init__(self):
         self.tmp = "./tmp"
@@ -20,13 +17,14 @@ class VideoStream():
     def write(self, image, index):
         cv2.imwrite(os.path.join(self.tmp, f"{index:04}.png"), image)
     def save(self, fname):
-        cmd = f"ffmpeg -f image2 -i {self.tmp}/%04d.png -crf 0 -y {fname}"
+        encoding = '-c:v mpeg4 -q:v 10'
+        cmd = f"ffmpeg -f image2 -framerate 30 -i {self.tmp}/%04d.png {encoding} -y {fname}"
         subprocess.call(cmd, shell=True)
 
 def compressed_imgmsg_to_cv2(cmprs_img_msg):
     str_msg = cmprs_img_msg.data
     buf = np.ndarray(shape=(1, len(str_msg)),
-                        dtype=np.uint8, buffer=cmprs_img_msg.data)
+                        dtype=np.uint8, buffer=str_msg)
     im = cv2.imdecode(buf, cv2.IMREAD_UNCHANGED)
     return im
 
@@ -37,25 +35,22 @@ def compressed_imgmsg_to_cv2_old(cmprs_img_msg):
     vis_img = cv2.imread( 'tmp.jpeg' )
     return vis_img
 
-
 def bag_to_video(bag_path, output_dir):
     stream = VideoStream()
-    i_step = 0
-
     output_path = os.path.join(output_dir, 'camera_front.avi')
 
-    print('Starting reading bag file: {}'.format(bag_path))
-    bag = rosbag.Bag(bag_path, 'r')
-    msgs = bag.read_messages(topics=['/interfacea/link2/image/compressed'])
-
-    print('Starting to save video to: {}'.format(output_path))
+    print('Reading bag file: {}'.format(bag_path))
     start_time = time.perf_counter()
-    for m in msgs[:100]:
+    bag = rosbag.Bag(bag_path, 'r')
+    msgs = bag.read_messages(topics=[CAMERA_TOPIC])
+
+    print('Saving video to: {}'.format(output_path))
+    i_step = 0
+    for m in msgs:
         vis_img = compressed_imgmsg_to_cv2(m.message)
         stream.write(vis_img, index=i_step)
         i_step += 1
     end_time = time.perf_counter()
 
-    print(f"Time to extract: {end_time - start_time}")
-
     stream.save(output_path)
+    print(f"Time to extract & save: {end_time - start_time:.2f}s")
